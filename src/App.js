03f1c0b56e94b0b2b5ffc793691f65e89dfd068f
Mehmet Zorlu <memo.zzzorlu@gmail.com>
2022-09-12 13:32:14 +0200

    Trying another deploy 2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {BrowserRouter} from "react-router-dom";
 function App() {
    return (
       <div>
-         <BrowserRouter basename='/Portfolio'>
+         <BrowserRouter basename='/portfolio'>
             <BaseLayout/>
          </BrowserRouter>
       </div>
